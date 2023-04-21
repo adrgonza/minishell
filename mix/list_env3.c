f0e794945b82db5c447a40d2ce221d162aa2ae23
Andres Mejia <amejia@c1r11s4.42madrid.com>
2023-04-21 15:44:58 +0200

    antes de juntar todo

--- mix/list_env3.c
+++ mix/list_env3.c
@@ -6,7 +6,7 @@
 /*   By: amejia <amejia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 22:27:07 by amejia            #+#    #+#             */
-/*   Updated: 2023/04/20 22:46:20 by amejia           ###   ########.fr       */
+/*   Updated: 2023/04/21 00:01:51 by amejia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,18 @@ t_env *ft_envfind(t_env *token, char *name)
 	}
 	return (NULL);
 }
-
+//unset
 void ft_unsetkn(t_token *token)
 {
 	t_env	*result;
 
-	result = ft_envfind(g_state->env,token->args[1]);
+	result = ft_envfind(g_state.envp,token->args[1]);
 	if (result == NULL)
 		return ;
 	ft_envdelone(result);
 }
 
+//export
 void ft_settoken(t_token *token)
 {
 	t_env	*added;
@@ -43,5 +44,19 @@ void ft_settoken(t_token *token)
 		ft_printf("Memory Error");
 		return ;
 	}
-	ft_envadd_back(g_state->env,added);
+	ft_envadd_back(g_state.envp, added);
 }
+
+//env
+void ft_envprint(void)
+{
+	t_env	*env;
+
+	env = g_state.envp;
+	while (env != NULL)
+	{
+		ft_printf("%s=%s\n",env->name,env->args);
+		env = env->next;	
+	}
+}
+
