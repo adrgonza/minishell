0aac8e0275686645eac1d1ac217fff457c1fcca6
adrgonza <adrian.gonser@icloud.com>
2023-05-11 14:52:54 +0200

    490 cases

--- parsing/parsing_tools.c
+++ parsing/parsing_tools.c
@@ -6,7 +6,7 @@
 /*   By: adrgonza <adrgonza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 01:04:31 by adrgonza          #+#    #+#             */
-/*   Updated: 2023/05/11 14:24:42 by adrgonza         ###   ########.fr       */
+/*   Updated: 2023/05/11 14:49:00 by adrgonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,7 +84,7 @@ void	reordenate_tokens(t_token	**token)
 	while ((*token)->next)
 	{
 		if ((*token)->next && (*token)->type == T_COMMAND)
-			if ((*token)->next->next && ((*token)->next->type == T_LESS))
+			if ((*token)->next->next && ((*token)->next->type == T_LESS || (*token)->next->type == T_LESSLESS))
 				ft_tknswap_next(*token);
 		if ((*token)->next && ((*token)->type == T_GREAT || (*token)->type == T_GREATGREAT))
 			if ((*token)->next->next && (*token)->next->type == T_COMMAND)
